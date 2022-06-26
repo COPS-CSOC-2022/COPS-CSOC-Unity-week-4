c72a16ba83809bbfce81d398b02a7536d9ec19b7
Arunabh <arunabh.gupta.min20@itbhu.ac.in>
2022-06-26 19:29:13 +0530

    added some more instructions for task2

--- Task2/Scripts/Movement.cs
+++ Task2/Scripts/Movement.cs
@@ -9,26 +9,33 @@ public class Movement : MonoBehaviour
     // For this you have to take access of the rigidbody component of the player gameobject.
 
     public Rigidbody2D rb; // this is how you declare rigidbody variable
-    public float speed = 10;
-    
-    private void Start() {
-         rb = gameObject.GetComponent<Rigidbody2D>(); // this is how you can take access of the rigidbody of the player gameobject
+    public int coins = 0;
+    public float speed;
+    public float force_up = 100f;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>(); // this is how you can take access of the rigidbody of the player gameobject
     }
-    private void Update() {
-        float h = Input.GetAxis("Horizontal"); 
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         // just to get user input from wasd or arrow keys
 
         Vector2 player_position = transform.position;
-        
-        player_position.x += h * Time.deltaTime* speed; 
-       
+
+        player_position.x += h * Time.deltaTime * speed;
+
         transform.position = player_position;
 
         // here you wil add an upward force on your rb.
         // use  rb.AddForce(Vector2 force, ForceMode ), forcemode is the type of force you want to apply. It can be Impulsive or  
         // a simple force
     }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // check the collision with the coin gameObject here
+    }
 
-    
 }
